f329f6c5bf66a45d38baba1a57033c67da748c54
notrobot23 <notrobot23@gmail.com>
2022-06-04 21:56:09 -0400

    it do the scene load now

--- Assets/Scripts/play.cs
+++ Assets/Scripts/play.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
+using UnityEngine.SceneManagement;
 public class play : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void playGame()
     {
-        
+        SceneManager.LoadScene(1);
     }
-
-    // Update is called once per frame
-    void Update()
+    public void exit()
     {
+        print("exit");
+        Application.Quit();
         
     }
 }
