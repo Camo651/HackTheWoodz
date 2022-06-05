7b69fc72827a50242e397e888565aae2f5347b4e
notrobot23 <notrobot23@gmail.com>
2022-06-05 02:46:52 -0400

    intro screen/ui elements done

--- Assets/Scripts/play.cs
+++ Assets/Scripts/play.cs
@@ -3,8 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
+
 public class play : MonoBehaviour
 {
+    public GameObject playbutton;
+    public GameObject exitButton;
+    public GameObject instructions;
+    public GameObject backToMenu;
+    public GameObject controlsbutton; 
     public void playGame()
     {
         SceneManager.LoadScene(1);
@@ -14,5 +20,23 @@ public class play : MonoBehaviour
         print("exit");
         Application.Quit();
         
+
+    }
+    public void controls()
+    {
+        playbutton.SetActive(false);
+        exitButton.SetActive(false);
+        controlsbutton.SetActive(false);
+        instructions.SetActive(true);
+        backToMenu.SetActive(true);
+    }
+    public void menu()
+    {
+        playbutton.SetActive(true);
+        exitButton.SetActive(true);
+        instructions.SetActive(false);
+        backToMenu.SetActive(false);
+
+        controlsbutton.SetActive(true);
     }
 }
